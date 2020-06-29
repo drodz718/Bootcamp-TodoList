332e1841cf60700a7d7fe49153a0a1b323915271
Dylan Rodriguez <drodz718@gmail.com>
2020-06-29 18:26:46 -0400

    created functionality to add or remove todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,35 @@
+//for the on keyword, ul is used before the li keyword because it is definetly on the page
+// when it loads as opposed to an li which may or may have not been there
+
+
 // Check off specific Todos by clicking
-$("li").click(function(){
+
+//execute function only when an li inside of a ul is clicked
+//works for future added li too
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");    
 });
 
 //Click on x to delete todo
-$("span").click(function(event) {
+//executes function when any current or future spans inside of a ul is clicked
+$("ul").on("click", "span", function(event) {
     //deletes entire li (without parent(), only span would get deleted)
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    //checks for enter key
+    if (event.which === 13) {
+        //gets new todo text from input
+        let todoText = $(this).val();
+        //clear input
+        $(this).val("");
+        //create new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+
+        
+    }
 });
